0429b5e3d95ad93e096aeec6d4fe99d1e4aa310c
Arthemus Cicotti Moreira <arthemus.moreira@gmail.com>
2017-03-29 13:34:51 -0300

    Init project.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify');
+
+gulp.task('default', function () {
+    return gulp.src('cronapi.js')
+        .pipe(uglify())
+        .pipe(rename('cronapi.min.js'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'CronApi-JS build finished' }));
+});
