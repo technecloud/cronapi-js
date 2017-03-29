8ed6924f9f2045f162617f4a5b534bbcc083b1a7
tiagoromano <tiagoromano>
2017-03-29 14:50:10 -0300

    Adicionado estrutura de função no cronapi

--- cronapi.js
+++ cronapi.js
@@ -1,5 +1,34 @@
-(function () {
-    'use strict';
+(function() {
+	'use strict';
 
-    
+  window.cronapi = {
+  	/**
+  	 * @category Conversion
+  	 * categorySynonymous Conversão|Convert
+  	 */
+  	conversion : {
+  		/**
+    	 * @type function
+    	 * @name Texto para texto binário
+    	 * @nameSynonymous asciiToBinary
+    	 * @description Função para converter texto para texto binário
+    	 * @param {string} astring - The x value.
+    	 */
+  		asciiToBinary : function(astring) {
+  			var binary = "";
+  			if (astring.length > 0) {
+  				for (var i = 0; i < astring.length; i++) {
+  					var value = astring.charCodeAt(i);
+  					for (var j = 7; j >= 0; j--) {
+  						binary += ((value >> j) & 1);
+  					}
+  				}
+  			}
+  			return binary;
+  		}
+  	}
+  };
+
 })();
+
+
