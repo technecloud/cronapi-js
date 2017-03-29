a7a034e710e3d1af103fc669a57df3f3bd9fa95b
tiagoromano <tiagoromano>
2017-03-29 15:00:51 -0300

    mudança na estrutura do cronapi

--- cronapi.js
+++ cronapi.js
@@ -1,34 +1,29 @@
 (function() {
 	'use strict';
 
-  window.cronapi = {
-  	/**
-  	 * @category Conversion
-  	 * categorySynonymous Conversão|Convert
-  	 */
-  	conversion : {
-  		/**
-    	 * @type function
-    	 * @name Texto para texto binário
-    	 * @nameSynonymous asciiToBinary
-    	 * @description Função para converter texto para texto binário
-    	 * @param {string} astring - The x value.
-    	 */
-  		asciiToBinary : function(astring) {
-  			var binary = "";
-  			if (astring.length > 0) {
-  				for (var i = 0; i < astring.length; i++) {
-  					var value = astring.charCodeAt(i);
-  					for (var j = 7; j >= 0; j--) {
-  						binary += ((value >> j) & 1);
-  					}
-  				}
-  			}
-  			return binary;
-  		}
-  	}
-  };
-
+	window.cronapi = {};
+	/**
+	 * @category Conversion
+	 * categorySynonymous Conversão|Convert
+	 */
+	window.cronapi.conversion = {};
+	/**
+	 * @type function
+	 * @name Texto para texto binário
+	 * @nameSynonymous asciiToBinary
+	 * @description Função para converter texto para texto binário
+	 * @param {string} astring - The x value.
+	 */
+	window.cronapi.conversion.asciiToBinary = function(astring) {
+		var binary = "";
+		if (astring.length > 0) {
+			for (var i = 0; i < astring.length; i++) {
+				var value = astring.charCodeAt(i);
+				for (var j = 7; j >= 0; j--) {
+					binary += ((value >> j) & 1);
+				}
+			}
+		}
+		return binary;
+	};
 })();
-
-
