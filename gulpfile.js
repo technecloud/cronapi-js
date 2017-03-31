161a3268a7df5d0a2ea3dc94859fa1c128f926b3
tiagoromano <tiagoromano>
2017-03-31 15:37:24 -0300

    teste bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify');
+//gulp.task('default', gulp.series('minify', 'i18n'))
 
 gulp.task('default', function () {
     return gulp.src('cronapi.js')
@@ -11,8 +12,8 @@ gulp.task('default', function () {
         .pipe(gulp.dest('dist/'));
         //.pipe(notify({ message: 'CronApi-JS build finished' }));
 });
-gulp.task('i18n', function() {
+/*gulp.task('i18n', function() {
   return gulp.src('i18n/')
     .pipe(gulp.dest('i18n/'))
     .pipe(notify({ message: 'CronApi-JS build finished' }));
-});
+});*/
