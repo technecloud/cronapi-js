edd48b7d4cb952199349151a408124f9d7db43f3
Arthemus Cicotti Moreira <arthemus.moreira@gmail.com>
2017-03-31 15:51:34 -0300

    Novas tarefas e definições no gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,18 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify');
-//gulp.task('default', gulp.series('minify', 'i18n'))
 
-gulp.task('default', function () {
+gulp.task('minify', function () {
     return gulp.src('cronapi.js')
         .pipe(uglify())
         .pipe(rename('cronapi.min.js'))
-        .pipe(gulp.dest('dist/'))
-        .pipe(notify({ message: 'CronApi-JS build finished' }));
+        .pipe(gulp.dest('dist/'));
 });
-/*gulp.task('i18n', function() {
-  return gulp.src('i18n/')
-    .pipe(gulp.dest('i18n/'))
-    .pipe(notify({ message: 'CronApi-JS build finished' }));
-});*/
+
+gulp.task('i18n', function () {
+    return gulp.src('i18n/').pipe(gulp.dest('i18n/'));
+});
+
+gulp.task('build', ['minify', 'i18n']);
+
+gulp.task('default', ['build']);
