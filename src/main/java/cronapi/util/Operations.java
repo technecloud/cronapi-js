e20e2f19894130b395ae16e629666364ff75eaf2
tiagoromano <tiagoromano>
2017-04-17 16:03:42 -0300

    colocado o nameTags como Array

--- src/main/java/cronapi/util/Operations.java
+++ src/main/java/cronapi/util/Operations.java
@@ -25,8 +25,9 @@ public class Operations {
 	public Operations() {
 	}
 
-	@CronapiMetaData(type = "function", name = "{{copyTextToTransferAreaName}}", nameTags = "copyTextToTransferArea", description = "{{copyTextToTransferAreaDescription}}", params = {
-			"{{copyTextToTransferAreaParam0}}" }, paramsType = { ObjectType.STRING })
+	@CronapiMetaData(type = "function", name = "{{copyTextToTransferAreaName}}", nameTags = {
+			"copyTextToTransferArea" }, description = "{{copyTextToTransferAreaDescription}}", params = {
+					"{{copyTextToTransferAreaParam0}}" }, paramsType = { ObjectType.STRING })
 	public static final void copyTextToTransferArea(Var strVar) throws Exception {
 		String str = strVar.getObjectAsString();
 		java.awt.datatransfer.Clipboard clipboard = java.awt.Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -34,8 +35,10 @@ public class Operations {
 		clipboard.setContents(selection, null);
 	}
 
-	@CronapiMetaData(type = "function", name = "{{shellExecuteName}}", nameTags = "shellExecute ", description = "{{shellExecuteDescription}}", params = {
-			"{{shellExecuteParam0}}", "{{shellExecuteParam1}}" }, paramsType = { ObjectType.STRING, ObjectType.BOOLEAN }, returnType = ObjectType.STRING)
+	@CronapiMetaData(type = "function", name = "{{shellExecuteName}}", nameTags = {
+			"shellExecute" }, description = "{{shellExecuteDescription}}", params = { "{{shellExecuteParam0}}",
+					"{{shellExecuteParam1}}" }, paramsType = { ObjectType.STRING,
+							ObjectType.BOOLEAN }, returnType = ObjectType.STRING)
 	public static final Var shellExecute(Var cmdline, Var waitFor) throws Exception {
 		Boolean waitForCasted = (Boolean) waitFor.getObject();
 		Process p = Runtime.getRuntime().exec(cmdline.getObjectAsString());
@@ -53,14 +56,16 @@ public class Operations {
 	}
 
 	// Retorna um numério aleatório 
-	@CronapiMetaData(type = "function", name = "{{randomName}}", nameTags = "random", description = "{{randomDescription}}", params = {
-			"{{randomParam0}}" }, paramsType = { ObjectType.DOUBLE }, returnType = ObjectType.DOUBLE)
+	@CronapiMetaData(type = "function", name = "{{randomName}}", nameTags = {
+			"random" }, description = "{{randomDescription}}", params = {
+					"{{randomParam0}}" }, paramsType = { ObjectType.DOUBLE }, returnType = ObjectType.DOUBLE)
 	public static final Var random(Var maxValue) throws Exception {
 		return new Var(Math.round(Math.random() * maxValue.getObjectAsDouble()));
 	}
 
-	@CronapiMetaData(type = "function", name = "{{compressToZipName}}", nameTags = "compressToZip", description = "{{compressToZipDescription}}", params = {
-			"{{compressToZipParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
+	@CronapiMetaData(type = "function", name = "{{compressToZipName}}", nameTags = {
+			"compressToZip" }, description = "{{compressToZipDescription}}", params = {
+					"{{compressToZipParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
 	public static final Var compressToZip(Var value) throws Exception {
 		java.io.ByteArrayOutputStream output = new java.io.ByteArrayOutputStream();
 		java.util.zip.DeflaterOutputStream compresser = new java.util.zip.DeflaterOutputStream(output);
@@ -70,8 +75,9 @@ public class Operations {
 		return new Var(output.toByteArray());
 	}
 
-	@CronapiMetaData(type = "function", name = "{{decodeZipFromByteName}}", nameTags = "decodeZipFromByte", description = "{{decodeZipFromByteDescription}}", params = {
-			"{{decodeZipFromByteParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
+	@CronapiMetaData(type = "function", name = "{{decodeZipFromByteName}}", nameTags = {
+			"decodeZipFromByte" }, description = "{{decodeZipFromByteDescription}}", params = {
+					"{{decodeZipFromByteParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
 	public static final Var decodeZipFromByte(Var value) throws Exception {
 		java.io.ByteArrayInputStream input = new java.io.ByteArrayInputStream((byte[]) value.getObject());
 		java.util.zip.InflaterInputStream decompresser = new java.util.zip.InflaterInputStream(input);
@@ -86,23 +92,23 @@ public class Operations {
 		input.close();
 		return new Var(out.toByteArray());
 	}
-	
+
 	@CronapiMetaData(type = "function", name = "{{sleep}}", nameTags = {
 			"sleep" }, description = "{{functionToSleep}}", params = {
-					"{{timeSleepInSecond}}" }, paramsType = {ObjectType.LONG }, returnType = ObjectType.VOID)
+					"{{timeSleepInSecond}}" }, paramsType = { ObjectType.LONG }, returnType = ObjectType.VOID)
 	public static final void sleep(Var time) throws Exception {
-	  long sleepTime = (time.getObjectAsLong() * 1000);
-	  Thread.sleep(sleepTime);
+		long sleepTime = (time.getObjectAsLong() * 1000);
+		Thread.sleep(sleepTime);
 	}
-	
+
 	@CronapiMetaData(type = "function", name = "{{throwException}}", nameTags = {
 			"throwException" }, description = "{{functionToThrowException}}", params = {
-					"{{exceptionToBeThrow}}" }, paramsType = {ObjectType.OBJECT }, returnType = ObjectType.VOID)
-	public static final void throwException(Var exception) throws Exception { 
-    if (exception.getObject() instanceof Exception)
-      throw Exception.class.cast(exception.getObject());
-    else if (exception.getObject() instanceof String) 
-      throw new Exception(exception.getObjectAsString());
-  }
+					"{{exceptionToBeThrow}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.VOID)
+	public static final void throwException(Var exception) throws Exception {
+		if (exception.getObject() instanceof Exception)
+			throw Exception.class.cast(exception.getObject());
+		else if (exception.getObject() instanceof String)
+			throw new Exception(exception.getObjectAsString());
+	}
 
 }
