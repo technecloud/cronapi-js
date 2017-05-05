12433cd0881317de55a072270eca90cc3def5b91
drigores <drigores>
2017-05-05 10:37:00 -0300

    Improvement: Adição de funções de soma, subtração e multiplicação.

--- src/main/java/cronapi/math/Operations.java
+++ src/main/java/cronapi/math/Operations.java
@@ -1,3 +1,9 @@
+package cronapi.math;
+
+import cronapi.CronapiMetaData;
+import cronapi.Var;
+import cronapi.CronapiMetaData.CategoryType;
+import cronapi.CronapiMetaData.ObjectType;
 
 /**
  * Classe que representa operações matemáticas
@@ -10,13 +16,85 @@
 @CronapiMetaData(category = CategoryType.UTIL, categoryTags = { "Math", "Matemática" })
 public class Operations {
 
+	@CronapiMetaData(type = "function", name = "{{multiplyFunctionName}}", nameTags = {
+			"multiplyFunction" }, description = "{{multiplyFunctionDescription}}", params = {
+					"{{multiplyFunctionParam0}}" }, paramsType = {
+							ObjectType.OBJECT }, returnType = ObjectType.OBJECT, arbitraryParams = true)
+	public static final Var multiply(Var... values) throws Exception {
+		Var result = new Var();
+
+		switch (values[0].getType()) {
+		case DOUBLE: {
+			result = multiplyDouble(values);
+			break;
+		}
+		case LONG: {
+			result = multiplyLong(values);
+			break;
+		}
+		default: {
+			result = multiplyDouble(values);
+		}
+
+		}
+		return result;
+	}
+
+	@CronapiMetaData(type = "function", name = "{{subtractFunctionName}}", nameTags = {
+			"subtractFunction" }, description = "{{subtractFunctionDescription}}", params = {
+					"{{subtractFunctionParam0}}" }, paramsType = {
+							ObjectType.OBJECT }, returnType = ObjectType.OBJECT, arbitraryParams = true)
+	public static final Var subtract(Var... values) throws Exception {
+		Var result = new Var();
+
+		switch (values[0].getType()) {
+		case DOUBLE: {
+			result = subtractDouble(values);
+			break;
+		}
+		case LONG: {
+			result = subtractLong(values);
+			break;
+		}
+		default: {
+			result = subtractDouble(values);
+		}
+
+		}
+		return result;
+	}
+
+	@CronapiMetaData(type = "function", name = "{{addFunctionName}}", nameTags = {
+			"addFunction" }, description = "{{addFunctionDescription}}", params = {
+					"{{addFunctionParam0}}" }, paramsType = {
+							ObjectType.OBJECT }, returnType = ObjectType.OBJECT, arbitraryParams = true)
+	public static final Var add(Var... values) throws Exception {
+		Var result = new Var();
+
+		switch (values[0].getType()) {
+		case DOUBLE: {
+			result = addDouble(values);
+			break;
+		}
+		case LONG: {
+			result = addLong(values);
+			break;
+		}
+		default: {
+			result = addDouble(values);
+		}
+
+		}
+		return result;
+	}
+
 	@CronapiMetaData(type = "function", name = "{{addLongName}}", nameTags = {
 			"addLong" }, description = "{{addLongDescription}}", params = { "{{addLongParam0}}" }, paramsType = {
 					ObjectType.OBJECT }, returnType = ObjectType.LONG, arbitraryParams = true)
 	public static final Var addLong(Var... values) throws Exception {
-		Long addedValue = 0.0;
+		Long addedValue = 0L;
 		for (Var value : values) {
-			addedValue += values.getObjectAsLong();
+			addedValue += value.getObjectAsLong();
 		}
 		return new Var(addedValue);
 	}
@@ -27,7 +105,7 @@ public class Operations {
 	public static final Var addDouble(Var... values) throws Exception {
 		Double addedValue = 0.0;
 		for (Var value : values) {
-			addedValue += values.getObjectAsDouble();
+			addedValue += value.getObjectAsDouble();
 		}
 		return new Var(addedValue);
 	}
@@ -39,7 +117,8 @@ public class Operations {
 	public static final Var subtractLong(Var... values) throws Exception {
 		Long initialValue = values[0].getObjectAsLong();
 		for (int i = 1; i < values.length; i++) {
-			initialValue -= values.getObjectAsLong();
+			Var value = values[i];
+			initialValue -= value.getObjectAsLong();
 		}
 		return new Var(initialValue);
 	}
@@ -51,7 +130,8 @@ public class Operations {
 	public static final Var subtractDouble(Var... values) throws Exception {
 		Double initialValue = values[0].getObjectAsDouble();
 		for (int i = 1; i < values.length; i++) {
-			initialValue -= values.getObjectAsDouble();
+			Var value = values[i];
+			initialValue -= value.getObjectAsDouble();
 		}
 		return new Var(initialValue);
 	}
@@ -61,9 +141,9 @@ public class Operations {
 					"{{multiplyLongParam0}}" }, paramsType = {
 							ObjectType.OBJECT }, returnType = ObjectType.LONG, arbitraryParams = true)
 	public static final Var multiplyLong(Var... values) throws Exception {
-		Long returnValue = 1.0;
+		Long returnValue = 1L;
 		for (Var value : values) {
-			returnValue *= values.getObjectAsLong();
+			returnValue *= value.getObjectAsLong();
 		}
 		return new Var(returnValue);
 	}
@@ -75,9 +155,8 @@ public class Operations {
 	public static final Var multiplyDouble(Var... values) throws Exception {
 		Double returnValue = 1.0;
 		for (Var value : values) {
-			returnValue *= values.getObjectAsDouble();
+			returnValue *= value.getObjectAsDouble();
 		}
 		return new Var(returnValue);
 	}
-
 }
