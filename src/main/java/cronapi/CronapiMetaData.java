3810c7c4a691071b4ae6175f4ec00668d2e28364
Thiago Costa <thiagoalvescosta@gmail.com>
2017-05-11 10:44:06 -0300

    Correções na geração de código JAVA

--- src/main/java/cronapi/CronapiMetaData.java
+++ src/main/java/cronapi/CronapiMetaData.java
@@ -6,37 +6,37 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value={ElementType.METHOD, ElementType.TYPE})
+@Target(value = { ElementType.METHOD, ElementType.TYPE })
 public @interface CronapiMetaData {
-	String type()  default "";
-
-	boolean external() default true;
-
-	CategoryType category() default CategoryType.OTHER;
-
-	String[] categoryTags() default "";
-
-	String name() default "";
-
-	String[] nameTags() default "";
-
-	String description() default "";
-
-	String[] params() default "";
-	
-	String[] wizard() default "";
-	
-	boolean arbitraryParams()  default false;
-
-	ObjectType[] paramsType() default { ObjectType.UNKNOWN };
-
-	ObjectType returnType() default ObjectType.VOID;
-
-	public enum CategoryType {
-    CONVERSION, IO, UTIL, XML, DATETIME, DATABASE, EMAIL, FTP, OTHER
-	}
-
-	public enum ObjectType {
-		BOOLEAN, LONG, DOUBLE, DATETIME, STRING, LIST, MAP, DATASET, JSON, XML, OBJECT, UNKNOWN, VOID
-	}
+  String type() default "";
+  
+  boolean external() default true;
+  
+  CategoryType category() default CategoryType.OTHER;
+  
+  String[] categoryTags() default "";
+  
+  String name() default "";
+  
+  String[] nameTags() default "";
+  
+  String description() default "";
+  
+  String[] params() default "";
+  
+  String[] wizard() default "";
+  
+  boolean arbitraryParams() default false;
+  
+  ObjectType[] paramsType() default { ObjectType.UNKNOWN };
+  
+  ObjectType returnType() default ObjectType.VOID;
+  
+  public enum CategoryType {
+    IO, DATABASE, FRONTEND, CONVERSION, DATETIME, XML, EMAIL, FTP, JSON, LOGIC, TEXT, LIST, MAP, COLOR, LOOP, MATH, GRID, PRINT, OTHER, SEMAPHORE, UTIL, TREE
+  }
+  
+  public enum ObjectType {
+    BOOLEAN, LONG, DOUBLE, DATETIME, STRING, LIST, MAP, DATASET, JSON, XML, OBJECT, UNKNOWN, VOID
+  }
 }
