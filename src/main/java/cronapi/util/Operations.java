b2d90b5f166926b8da14eb4698f55f29e556bc27
drigores <drigores>
2017-05-19 15:04:08 -0300

    Improvement: Adição de anotações e mensagens

--- src/main/java/cronapi/util/Operations.java
+++ src/main/java/cronapi/util/Operations.java
@@ -14,7 +14,7 @@ import cronapi.i18n.Messages;
 /**
  * Classe que representa ...
  *
- * @author Rodrigo Reis
+ * @author Autor de Teste
  * @version 1.0
  * @since 2017-03-31
  *
@@ -30,7 +30,7 @@ public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{copyTextToTransferAreaName}}", nameTags = {
 			"copyTextToTransferArea" }, description = "{{copyTextToTransferAreaDescription}}", params = {
-			"{{copyTextToTransferAreaParam0}}" }, paramsType = { ObjectType.STRING })
+					"{{copyTextToTransferAreaParam0}}" }, paramsType = { ObjectType.STRING })
 	public static final void copyTextToTransferArea(Var strVar) throws Exception {
 		String str = strVar.getObjectAsString();
 		java.awt.datatransfer.Clipboard clipboard = java.awt.Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -40,8 +40,8 @@ public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{shellExecuteName}}", nameTags = {
 			"shellExecute" }, description = "{{shellExecuteDescription}}", params = { "{{shellExecuteParam0}}",
-			"{{shellExecuteParam1}}" }, paramsType = { ObjectType.STRING,
-			ObjectType.BOOLEAN }, returnType = ObjectType.STRING)
+					"{{shellExecuteParam1}}" }, paramsType = { ObjectType.STRING,
+							ObjectType.BOOLEAN }, returnType = ObjectType.STRING)
 	public static final Var shellExecute(Var cmdline, Var waitFor) throws Exception {
 		Boolean waitForCasted = (Boolean) waitFor.getObject();
 		Process p = Runtime.getRuntime().exec(cmdline.getObjectAsString());
@@ -61,14 +61,14 @@ public class Operations {
 	// Retorna um numério aleatório
 	@CronapiMetaData(type = "function", name = "{{randomName}}", nameTags = {
 			"random" }, description = "{{randomDescription}}", params = {
-			"{{randomParam0}}" }, paramsType = { ObjectType.DOUBLE }, returnType = ObjectType.DOUBLE)
+					"{{randomParam0}}" }, paramsType = { ObjectType.DOUBLE }, returnType = ObjectType.DOUBLE)
 	public static final Var random(Var maxValue) throws Exception {
 		return new Var(Math.round(Math.random() * maxValue.getObjectAsDouble()));
 	}
 
 	@CronapiMetaData(type = "function", name = "{{compressToZipName}}", nameTags = {
 			"compressToZip" }, description = "{{compressToZipDescription}}", params = {
-			"{{compressToZipParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
+					"{{compressToZipParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
 	public static final Var compressToZip(Var value) throws Exception {
 		java.io.ByteArrayOutputStream output = new java.io.ByteArrayOutputStream();
 		java.util.zip.DeflaterOutputStream compresser = new java.util.zip.DeflaterOutputStream(output);
@@ -80,7 +80,7 @@ public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{decodeZipFromByteName}}", nameTags = {
 			"decodeZipFromByte" }, description = "{{decodeZipFromByteDescription}}", params = {
-			"{{decodeZipFromByteParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
+					"{{decodeZipFromByteParam0}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.OBJECT)
 	public static final Var decodeZipFromByte(Var value) throws Exception {
 		java.io.ByteArrayInputStream input = new java.io.ByteArrayInputStream((byte[]) value.getObject());
 		java.util.zip.InflaterInputStream decompresser = new java.util.zip.InflaterInputStream(input);
@@ -98,7 +98,7 @@ public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{sleep}}", nameTags = {
 			"sleep" }, description = "{{functionToSleep}}", params = {
-			"{{timeSleepInSecond}}" }, paramsType = { ObjectType.LONG }, returnType = ObjectType.VOID)
+					"{{timeSleepInSecond}}" }, paramsType = { ObjectType.LONG }, returnType = ObjectType.VOID)
 	public static final void sleep(Var time) throws Exception {
 		long sleepTime = (time.getObjectAsInt() * 1000);
 		Thread.sleep(sleepTime);
@@ -106,7 +106,7 @@ public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{throwException}}", nameTags = {
 			"throwException" }, description = "{{functionToThrowException}}", params = {
-			"{{exceptionToBeThrow}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.VOID)
+					"{{exceptionToBeThrow}}" }, paramsType = { ObjectType.OBJECT }, returnType = ObjectType.VOID)
 	public static final void throwException(Var exception) throws Exception {
 		if (exception.getObject() instanceof Exception)
 			throw Exception.class.cast(exception.getObject());
@@ -114,18 +114,26 @@ public class Operations {
 			throw new Exception(exception.getObjectAsString());
 	}
 
+	@CronapiMetaData(type = "function", name = "{{createExceptionName}}", nameTags = {
+			"createException" }, description = "{{createExceptionName}}", params = {
+					"{{createExceptionParam0}}" }, paramsType = { ObjectType.STRING }, returnType = ObjectType.OBJECT)
+	public static final Var createException(Var msg) throws Exception {
+		Exception e = new Exception(msg.getObjectAsString());
+		return new Var(e);
+	}
+
 	@CronapiMetaData(type = "function", name = "{{callBlockly}}", nameTags = {
 			"callBlockly" }, description = "{{functionToCallBlockly}}", params = { "{{classNameWithMethod}}",
-			"{{params}}" }, wizard = "procedures_callblockly_callnoreturn", paramsType = { ObjectType.OBJECT,
-			ObjectType.OBJECT }, returnType = ObjectType.VOID, arbitraryParams = true)
+					"{{params}}" }, wizard = "procedures_callblockly_callnoreturn", paramsType = { ObjectType.OBJECT,
+							ObjectType.OBJECT }, returnType = ObjectType.VOID, arbitraryParams = true)
 	public static final void callBlocklyNoReturn(Var classNameWithMethod, Var... params) throws Exception {
 		callBlockly(classNameWithMethod, params);
 	}
 
 	@CronapiMetaData(type = "function", name = "{{callBlockly}}", nameTags = {
 			"callBlockly" }, description = "{{functionToCallBlockly}}", params = { "{{classNameWithMethod}}",
-			"{{params}}" }, wizard = "procedures_callblockly_callreturn", paramsType = { ObjectType.OBJECT,
-			ObjectType.OBJECT }, returnType = ObjectType.OBJECT, arbitraryParams = true)
+					"{{params}}" }, wizard = "procedures_callblockly_callreturn", paramsType = { ObjectType.OBJECT,
+							ObjectType.OBJECT }, returnType = ObjectType.OBJECT, arbitraryParams = true)
 	public static final Var callBlockly(Var classNameWithMethod, Var... params) throws Exception {
 
 		String className = classNameWithMethod.getObjectAsString();
@@ -138,7 +146,7 @@ public class Operations {
 		Class clazz = Class.forName(className);
 
 		Method methodToCall = clazz.getMethods()[0];
-		for (Method m: clazz.getMethods()) {
+		for (Method m : clazz.getMethods()) {
 			if (m.getName().equalsIgnoreCase(method)) {
 				methodToCall = m;
 				break;
@@ -157,10 +165,10 @@ public class Operations {
 			}
 		}
 
-    CronapiMetaData annotation = (CronapiMetaData) clazz.getAnnotation(CronapiMetaData.class);
+		CronapiMetaData annotation = (CronapiMetaData) clazz.getAnnotation(CronapiMetaData.class);
 		if (annotation == null || (annotation != null && !"blockly".equals(annotation.type()))) {
-      throw new Exception(Messages.getString("accessDenied"));
-    }
+			throw new Exception(Messages.getString("accessDenied"));
+		}
 
 		Object o = methodToCall.invoke(clazz, callParams);
 
