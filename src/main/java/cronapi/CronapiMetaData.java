64b0f06f9406528c7eda6c16ffe8df965245702c
tiagoromano <tiagoromano>
2017-05-19 10:13:35 -0300

    Definido o type padrao para function no Cronapimetadata Adicionado description em ParamMetaData

--- src/main/java/cronapi/CronapiMetaData.java
+++ src/main/java/cronapi/CronapiMetaData.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = { ElementType.METHOD, ElementType.TYPE })
 public @interface CronapiMetaData {
-  String type() default "";
+  String type() default "function";
   
   boolean external() default true;
   
