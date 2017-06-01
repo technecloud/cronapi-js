51a00c8c0ba5dfc008178e5bf7188b970b4383b7
Thiago Costa <thiagoalvescosta@gmail.com>
2017-06-01 10:41:50 -0300

    Suporte a request

--- src/main/java/cronapi/CronapiFilter.java
+++ src/main/java/cronapi/CronapiFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import cronapi.rest.CallBlocklyREST;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,9 @@ import cronapi.i18n.Messages;
 
 @Component
 public class CronapiFilter implements Filter {
+    public static ThreadLocal<HttpServletRequest> REQUEST = new ThreadLocal<>();
+    public static ThreadLocal<HttpServletResponse> RESPONSE = new ThreadLocal<>();
+
     @Override
     public void init(FilterConfig chain) throws ServletException {
 
@@ -20,11 +24,19 @@ public class CronapiFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        REQUEST.set(request);
+        RESPONSE.set(response);
+
         Messages.set(req.getLocale());
         try {
           chain.doFilter(req, resp);
         } finally {
           RestClient.removeClient();
+          REQUEST.remove();
+          RESPONSE.remove();
+          Messages.remove();
         }
     }
 
