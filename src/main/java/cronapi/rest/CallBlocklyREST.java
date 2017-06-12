a105d0e97c4decc2626c8a52e1f7a10bf5595b8c
tarsomatos <tarsomatos@gmail.com>
2017-06-12 15:56:51 -0300

    Removido import de app.entity.User

--- src/main/java/cronapi/rest/CallBlocklyREST.java
+++ src/main/java/cronapi/rest/CallBlocklyREST.java
@@ -6,22 +6,27 @@ import java.util.concurrent.Callable;
 
 import javax.servlet.http.HttpServletRequest;
 
-import app.entity.User;
-import cronapi.*;
-import cronapi.database.DataSource;
-import cronapi.database.Operations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import cronapi.ErrorResponse;
+import cronapi.RestBody;
+import cronapi.RestClient;
+import cronapi.RestResult;
+import cronapi.Var;
+import cronapi.database.DataSource;
 import cronapi.database.TransactionManager;
 
 @RestController
