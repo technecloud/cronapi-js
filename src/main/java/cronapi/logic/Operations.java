d8282047e89f741927362b7aec4db7e664e6f55d
tarsomatos <tarsomatos@gmail.com>
2017-09-04 14:36:53 -0300

    Funções de logica sem descrição do parametro

--- src/main/java/cronapi/logic/Operations.java
+++ src/main/java/cronapi/logic/Operations.java
@@ -1,22 +1,24 @@
 package cronapi.logic;
 
-import cronapi.*;
+import cronapi.CronapiMetaData;
 import cronapi.CronapiMetaData.CategoryType;
 import cronapi.CronapiMetaData.ObjectType;
+import cronapi.ParamMetaData;
+import cronapi.Var;
 
 @CronapiMetaData(category = CategoryType.LOGIC, categoryTags = { "Lógica", "Logic" })
 public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{isNullName}}", nameTags = {
 			"isNullFunction" }, description = "{{isNullDescription}}", displayInline = true, returnType = ObjectType.BOOLEAN)
-	public static final Var isNull(@ParamMetaData(type = ObjectType.OBJECT, description = "") Var var)
+	public static final Var isNull(@ParamMetaData(type = ObjectType.OBJECT, description = "{{parameter}}") Var var)
 			throws Exception {
 		return (var.equals(Var.VAR_NULL)) ? Var.VAR_TRUE : Var.VAR_FALSE;
 	}
 
 	@CronapiMetaData(type = "function", name = "{{isNullOrEmptyName}}", nameTags = {
 			"isNullOrEmptyFunction" }, description = "{{isNullOrEmptyDescription}}", displayInline = true, returnType = ObjectType.BOOLEAN)
-	public static final Var isNullOrEmpty(@ParamMetaData(type = ObjectType.OBJECT, description = "") Var var)
+	public static final Var isNullOrEmpty(@ParamMetaData(type = ObjectType.OBJECT, description = "{{parameter}}") Var var)
 			throws Exception {
 		return (var.equals(Var.VAR_NULL) || var.getObjectAsString() == ""
 				|| (var.getType().equals(Var.Type.LIST) && var.getObjectAsList().isEmpty())) ? Var.VAR_TRUE
@@ -25,7 +27,7 @@ public class Operations {
 
 	@CronapiMetaData(type = "function", name = "{{isEmptyName}}", nameTags = {
 			"isEmptyFunction" }, description = "{{isEmptyDescription}}", displayInline = true, returnType = ObjectType.BOOLEAN)
-	public static final Var isEmpty(@ParamMetaData(type = ObjectType.OBJECT, description = "") Var var)
+	public static final Var isEmpty(@ParamMetaData(type = ObjectType.OBJECT, description = "{{parameter}}") Var var)
 			throws Exception {
 		return (var.getObjectAsString().isEmpty()
 				|| (var.getType().equals(Var.Type.LIST) && var.getObjectAsList().isEmpty())) ? Var.VAR_TRUE
