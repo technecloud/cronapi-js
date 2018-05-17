809fef5bec8fe21d5a2036990377c6af83662b7d
Thiago Costa <thiagoalvescosta@gmail.com>
2018-05-17 10:34:41 -0300

    Evoluçao

--- src/main/java/cronapi/odata/server/ODataServletV4.java
+++ src/main/java/cronapi/odata/server/ODataServletV4.java
@@ -1,5 +1,8 @@
 package cronapi.odata.server;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import cronapi.QueryManager;
 import org.apache.olingo.commons.api.ex.ODataException;
 import org.apache.olingo.jpa.metadata.api.JPAEdmExtension;
 import org.apache.olingo.jpa.metadata.core.edm.mapper.impl.IntermediateEntitySet;
@@ -9,14 +12,20 @@ import org.apache.olingo.jpa.processor.core.database.JPADefaultDatabaseProcessor
 import org.apache.olingo.jpa.processor.core.mapping.JPAPersistenceAdapter;
 import org.apache.olingo.jpa.processor.core.mapping.ResourceLocalPersistenceAdapter;
 import org.apache.olingo.server.api.processor.Processor;
+import org.eclipse.persistence.internal.jpa.parsing.jpql.JPQLParser;
+import org.eclipse.persistence.jpa.JpaQuery;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 
@@ -63,11 +72,45 @@ public class ODataServletV4 extends HttpServlet {
     handler.setJPAEdmExtension(new JPAEdmExtension() {
       @Override
       public void extendJPAEntitySet(Map<String, IntermediateEntitySet> set) {
-        try {
-          createDataSource(set, "Thiago", "app.entity.Company");
-        } catch (Exception e) {
-          e.printStackTrace();
+        JsonObject queries = QueryManager.getJSON();
+
+        for (Map.Entry<String, JsonElement> entry : queries.entrySet()) {
+          JsonObject customObj = entry.getValue().getAsJsonObject();
+          if (!QueryManager.isNull(customObj.get("entityFullName"))) {
+            String clazz = customObj.get("entityFullName").getAsString();
+            if (clazz.startsWith(namespace)) {
+              try {
+                createDataSource(set, entry.getKey(), clazz);
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          }
         }
+      }
+
+      @Override
+      public TypedQuery createQuery(EntityManager em, CriteriaQuery<Tuple> criteria) {
+        JPQLParser parser = JPQLParser.buildParserFor("select c from Teste c where c.Id = 'asdsad' ");
+        parser.parse();
+
+
+        //parser.getParseTree().getWhereNode()
+
+        //criteria.
+      //  TypedQuery<Tuple> query = em.createQuery(
+
+        //    "select c.id as id, c.id2 as id2 from Teste c", Tuple.class);
+
+       // List<Tuple> results = query.getResultList();
+
+        TypedQuery<Tuple> xy = em.createQuery(criteria);
+
+        String jpql = xy.unwrap(JpaQuery.class).getDatabaseQuery().getJPQLString();
+
+        return em.createQuery(criteria);
+
+        //return super.createQuery(criteria);
       }
     });
 
