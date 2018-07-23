9f86451d8cd83b682b580c68625e11ec38e047e1
Thiago Costa <thiagoalvescosta@gmail.com>
2018-07-23 12:03:46 -0300

    Melhorias no dataSourceMap

--- src/main/java/cronapi/rest/DataSourceMapREST.java
+++ src/main/java/cronapi/rest/DataSourceMapREST.java
@@ -2,17 +2,25 @@ package cronapi.rest;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import cronapi.AppConfig;
 import cronapi.QueryManager;
-import java.io.PrintWriter;
-import java.lang.management.ManagementFactory;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import cronapi.odata.server.ODataConfiguration;
+import org.eclipse.persistence.internal.jpa.deployment.PersistenceUnitProcessor;
+import org.eclipse.persistence.internal.jpa.deployment.SEPersistenceUnitInfo;
+import org.eclipse.persistence.jpa.Archive;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(value = "/js/dataSourceMap.js")
@@ -25,7 +33,7 @@ public class DataSourceMapREST {
   /**
    * Construtor
    **/
-  public DataSourceMapREST (){
+  public DataSourceMapREST() {
   }
 
 
@@ -34,9 +42,9 @@ public class DataSourceMapREST {
     response.setContentType("application/javascript");
     PrintWriter out = response.getWriter();
 
-    if(mapped == null) {
-      synchronized(DataSourceMapREST.class) {
-        if(mapped == null) {
+    if (mapped == null) {
+      synchronized (DataSourceMapREST.class) {
+        if (mapped == null) {
           HashMap<String, DataSourceDetail> mappedAllDs = new HashMap<String, DataSourceDetail>();
           JsonObject customQuery = QueryManager.getJSON();
           for (Map.Entry<String, JsonElement> entry : customQuery.entrySet()) {
@@ -45,10 +53,30 @@ public class DataSourceMapREST {
             mappedAllDs.put(guid, detail);
           }
 
-          if(!isDebug) {
-            mapped = mappedAllDs;
+          if (AppConfig.exposeLocalEntities()) {
+            Set<Archive> archives = PersistenceUnitProcessor.findPersistenceArchives();
+
+            for (Archive archive : archives) {
+              List<SEPersistenceUnitInfo> persistenceUnitInfos = PersistenceUnitProcessor.getPersistenceUnits(archive, Thread.currentThread().getContextClassLoader());
+              for (SEPersistenceUnitInfo pui : persistenceUnitInfos) {
+
+                String namespace = pui.getPersistenceUnitName();
+                for (String clazz : pui.getManagedClassNames()) {
+                  String clazzName = clazz.substring(clazz.lastIndexOf(".") + 1);
+                  String serviceUrlODATA = String.format(ODataConfiguration.SERVICE_URL + "%s/%s", namespace, clazzName);
+                  String serviceUrlApi = String.format("api/cronapi/crud/%s", clazz);
+
+                  DataSourceDetail detail = new DataSourceDetail(namespace, clazz, serviceUrlApi, serviceUrlODATA, true);
+                  mappedAllDs.put(clazz.replace(".entity.", "."), detail);
+                }
+              }
+            }
           }
-          else {
+
+
+          if (!isDebug) {
+            mapped = mappedAllDs;
+          } else {
             write(out, mappedAllDs);
           }
 
@@ -73,14 +101,13 @@ public class DataSourceMapREST {
 
     if ("entityFullName".equals(json.get("sourceType").getAsString())) {
       String entityFullName = json.get("entityFullName").getAsString();
+      String namespace = entityFullName.substring(0, entityFullName.indexOf(".entity."));
       String[] splited = entityFullName.split(Pattern.quote("."));
       String rootPackage = splited[0];
-      String serviceUrlODATA = String.format("api/cronapi/odata/v2/%s", rootPackage);
-      serviceUrlODATA = serviceUrl.replace("api/cronapi/query", serviceUrlODATA);
-      detail = new DataSourceDetail(customId, serviceUrl, serviceUrlODATA);
-    }
-    else {
-      detail = new DataSourceDetail(customId, serviceUrl, "");
+      String serviceUrlODATA = String.format(ODataConfiguration.SERVICE_URL + "%s", rootPackage);
+      detail = new DataSourceDetail(namespace, customId, serviceUrl, serviceUrlODATA, false);
+    } else {
+      detail = new DataSourceDetail("", customId, serviceUrl, "", false);
     }
     return detail;
   }
@@ -88,7 +115,7 @@ public class DataSourceMapREST {
   private void write(PrintWriter out, Map<String, DataSourceDetail> mapped) {
     out.println("window.dataSourceMap = window.dataSourceMap || [];");
 
-    mapped.forEach((k,v) -> {
+    mapped.forEach((k, v) -> {
 
       String curr = String.format("window.dataSourceMap[\"%s\"] = { customId: \"%s\", serviceUrl: \"%s\", serviceUrlODATA: \"%s\" };",
           k,
@@ -99,29 +126,36 @@ public class DataSourceMapREST {
 
       out.println(curr);
 
-      if (!v.customId.equals(k)) {
-        curr = String.format("window.dataSourceMap[\"%s\"] = { customId: \"%s\", serviceUrl: \"%s\", serviceUrlODATA: \"%s\" };",
-            v.customId,
-            v.customId,
-            v.serviceUrl,
-            v.serviceUrlODATA
-        );
+      if (!v.isEntity) {
+        if (!v.customId.equals(k)) {
+          curr = String.format("window.dataSourceMap[\"%s\"] = { customId: \"%s\", serviceUrl: \"%s\", serviceUrlODATA: \"%s\" };",
+              v.customId,
+              v.customId,
+              v.serviceUrl,
+              v.serviceUrlODATA
+          );
 
-        out.println(curr);
+
+          out.println(curr);
+        }
       }
     });
   }
 
   public class DataSourceDetail {
 
-    public DataSourceDetail(String customId, String serviceUrl, String serviceUrlODATA) {
+    public DataSourceDetail(String namespace, String customId, String serviceUrl, String serviceUrlODATA, boolean isEntity) {
+      this.namespace = namespace;
       this.customId = customId;
       this.serviceUrl = serviceUrl;
       this.serviceUrlODATA = serviceUrlODATA;
+      this.isEntity = isEntity;
     }
 
+    public String namespace;
     public String customId;
     public String serviceUrl;
     public String serviceUrlODATA;
+    public boolean isEntity;
   }
 }
