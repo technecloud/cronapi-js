c5d5deda778be3e5232bc46e85e03aea0587f734
Henrique Chang <henrique.queiroz@cronapp.io>
2018-12-12 11:25:49 -0300

    Construção dos objetos JSON

--- src/main/java/cronapi/rest/ImportBlocklyREST.java
+++ src/main/java/cronapi/rest/ImportBlocklyREST.java
@@ -8,8 +8,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import cronapi.TranslationManager;
 import cronapi.util.Operations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,17 +22,16 @@ public class ImportBlocklyREST {
   private static List<String> imports;
   private static boolean isDebug = Operations.IS_DEBUG;
   private final static List<String> API_PATHS = new ArrayList<String>(){{add("cronapi-js");add("cronapp-framework-js");add("cronapp-framework-mobile-js");}};
+  private static JsonObject localesJson = new JsonObject();
+  private static List<String> localesKeys = new ArrayList<String>();
+  private static JsonObject localesRef  = new JsonObject();
 
   private void fill(String base, File folder, List<String> imports) {
     for(File file : folder.listFiles(  (d,s) ->{
-   /*     var Teste = new Object() {
-          boolean test = false;
-        };*/
        boolean valid = true;
         for(String api : API_PATHS){
             if(d.getName().contains(api)){
                 valid = false;
-                //Teste.test = false;
                 break;
             }
         }
@@ -81,55 +80,57 @@ public class ImportBlocklyREST {
   }
 
   private void write(PrintWriter out, List<String> imports, String base, File folder) {
+    fillDefaultLanguages();
     for(File file : folder.listFiles(  (d,s) ->{
       boolean valid = true;
       for(String api : API_PATHS){
-        if(d.getName().equals("i18n")){
+        if(d.getName().contains("i18n")){
           valid = false;
           break;
         }
       }
       return valid;
     } )) {
-      if(file.getName().startsWith("i18n")) {
-        for(File filess : file.listFiles(  (d,s) ->{
-          boolean valid = true;
-          for(String api : API_PATHS){
-            if(d.getName().contains(api)){
-              valid = false;
-              break;
-            }
-          }
-          return valid;
-        } )) {
-          if(file.isDirectory()) {
-            fill(base, file, imports);
-          }
-          else {
-            if(file.getName().endsWith(".blockly.js")) {
-              String js = file.getAbsolutePath().replace(base, "");
-              js = js.replace("\\", "/");
-              if(js.startsWith("/")) {
-                js = js.substring(1);
-              }
-              imports.add(js + "?" + file.lastModified());
-            }
-          }
-        }
-      }
     }
-    JsonObject localesJson = TranslationManager.getJSON();
+    localesKeys = new ArrayList<String>();
+    String localesJsonString  = localesJson.toString() + ";";
+    String localesKeysString = arrayToString(localesKeys)  + ";";
+    String localesRefString = localesRef.toString()  + ";";
     out.println("window.blockly = window.blockly || {};");
     out.println("window.blockly.js = window.blockly.js || {};");
     out.println("window.blockly.js.blockly = window.blockly.js.blockly || {};");
     out.println("window.translations = window.translations || {};");
-    out.println("window.translations.locales = {\n" + "    'pt_br': 'Portugues (Brasil)',\n" + "    'en_us': 'English'\n" + "};");
-    out.println("window.translations.localesKeys = ['pt_br', 'en_us'];");
-    out.println("window.translations.localesRef = {\n" + "    'en*': 'en_us',\n" + "    'pt*': 'pt_br',\n" + "    '*': 'pt_br'\n" + "};");
+    out.println("window.translations.locales = " + localesJsonString);
+    out.println("window.translations.localesKeys = " + localesKeysString);
+    out.println("window.translations.localesRef =  " + localesRefString);
 
     for(String js : imports) {
       out.println("document.write(\"<script src='" + js + "'></script>\")");
     }
   }
 
+  private void fillDefaultLanguages(){
+    localesJson.addProperty("pt_br", "Portugues (Brasil)");
+    localesJson.addProperty("en_us", "English");
+
+    localesKeys.add("pt_br");
+    localesKeys.add("en_us");
+
+    localesRef.addProperty("en*", "en_us");
+    localesRef.addProperty("pt*", "pt_br");
+    localesRef.addProperty("*", "pt_br");
+  }
+
+  private String arrayToString(List<String> stringList){
+    StringBuilder b = new StringBuilder();
+    b.append("[");
+    for(String key : stringList){
+      if(stringList.indexOf(key) != 0) {
+        b.append(",");
+      }
+      b.append("'" + key + "'");
+    }
+    b.append("]");
+    return b.toString();
+  }
 }
