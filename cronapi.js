5d21668b167d89b6d4a050180e253bbb6564d7fb
Rodrigo Santos Reis <digo.santos.reis@gmail.com>
2019-03-21 11:32:50 -0300

    correçao de disable group

--- cronapi.js
+++ cronapi.js
@@ -1346,7 +1346,7 @@
    * @multilayer true
    */
   this.cronapi.screen.disableComponent = function(/** @type {ObjectType.OBJECT} @blockType ids_from_screen*/ id) {
-    if(id.indexOf('cron-crn-enterprise-combobox') >= 0){
+    if($('#'+id).data("kendoComboBox")){
       $('#'+id).data("kendoComboBox").enable(false);
     }else{
     $.each( $('#'+id).find('*').addBack(), function(index, value){ $(value).prop('disabled',true); });
