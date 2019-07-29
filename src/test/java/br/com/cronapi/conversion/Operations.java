6c7677c58e22ce346a5180a2ef16cf582bf3d915
Thiago Costa <thiagoalvescosta@gmail.com>
2019-07-29 11:41:47 -0300

    Suporte a conversao de vazio para double, int e long no Var

--- src/test/java/br/com/cronapi/conversion/Operations.java
+++ src/test/java/br/com/cronapi/conversion/Operations.java
@@ -10,8 +10,15 @@ import java.util.GregorianCalendar;
 public class Operations {
 
 
-    @Test
-    public void dateConversion(){
-        Assert.assertTrue(cronapi.conversion.Operations.convertLongToDate(Var.valueOf("1554914251430")).getObjectAsDateTime() instanceof GregorianCalendar);
-    }
+  @Test
+  public void dateConversion() {
+    Assert.assertTrue(cronapi.conversion.Operations.convertLongToDate(Var.valueOf("1554914251430")).getObjectAsDateTime() instanceof GregorianCalendar);
+  }
+
+  @Test
+  public void stringToIntConversion() {
+    Assert.assertTrue(Var.valueOf("").getObjectAsInt() == 0);
+    Assert.assertTrue(Var.valueOf("").getObjectAsDouble() == 0.0);
+    Assert.assertTrue(Var.valueOf("").getObjectAsLong() == 0L);
+  }
 }
