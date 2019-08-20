6ea1708c604cadfbe579b6f3e8a836ee660bba0e
Ricardo Caldas <ricardo@ricardombc.com.br>
2019-08-20 11:10:47 -0300

    Correção na chamada de blocos para o BPMN

--- src/main/java/cronapi/util/CallBlocklyOutside.java
+++ src/main/java/cronapi/util/CallBlocklyOutside.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Component;
 @Component("blockly")
 public class CallBlocklyOutside {
 
-    public static Object call(String className, String methodName, Object... args) throws Exception {
+    public static Object call(Object... args) throws Exception {
 
-        Var[] vars = new Var[args.length];
+        String className = (String) args[0];
+        String methodName = (String) args[1];
 
-        for (int i = 0; i < args.length; i++) {
-            vars[i] = Var.valueOf(args[i]);
+        Var[] vars = new Var[args.length - 2];
+
+        for (int i = 2; i < args.length; i++) {
+            vars[i-2] = Var.valueOf(args[i]);
         }
 
         Var result = cronapi.util.Operations.callBlockly(Var.valueOf(className + ":" + methodName), vars);
