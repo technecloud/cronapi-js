171e43a9aee813c53f970d28379074a0c682575f
Wesley Rover <wesley.rover@cronapp.io>
2019-09-11 13:48:52 -0300

    :ok_hand: Add class test in suite class test.

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -4,6 +4,7 @@ import br.com.cronapi.VarTest;
 import br.com.cronapi.dateTime.DateTimeTest;
 import br.com.cronapi.json.JsonTest;
 import br.com.cronapi.list.ListTest;
+import br.com.cronapi.math.MathTest;
 import br.com.cronapi.rest.security.CronappSecurityTest;
 import br.com.cronapi.xml.XmlTest;
 import org.junit.runner.RunWith;
@@ -13,6 +14,6 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         DateTimeTest.class, ListTest.class, CronappSecurityTest.class,
         DatabaseTest.class, RestClientTest.class, VarTest.class, JsonTest.class,
-        XmlTest.class})
+        XmlTest.class, MathTest.class})
 public class AllTests {
 }
