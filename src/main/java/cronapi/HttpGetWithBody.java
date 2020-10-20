65a50f413ef01c9bd16202a91c601ad35472c83b
Rodrigo Reis <digo.santos.reis@gmail.com>
2020-10-20 16:47:52 -0300

    Hotfix/support/2.7/2.7.4 to support/2.7 (#154)
    * Revert "CRONAPP-2633 Fonte de dados não traz dados corretamente em view pública (#149)"
    
    This reverts commit 6d79caef733713f78946c77118118d7e95020041.
    
    * CRONAPP-2431 Falha ao consumir webservice na IDE em relação ao Postman (#152)
    
    Co-authored-by: tiagoromano <tiago.romano@gmail.com>

--- src/main/java/cronapi/HttpGetWithBody.java
+++ src/main/java/cronapi/HttpGetWithBody.java
@@ -1,26 +1,15 @@
 package cronapi;
 
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpRequestBase;
 
-import java.net.URI;
-
-public class HttpGetWithBody extends HttpEntityEnclosingRequestBase {
+public class HttpGetWithBody extends HttpWithBody {
 
   public final static String METHOD_NAME = "GET";
 
-  public HttpGetWithBody() { super(); }
-
-  public HttpGetWithBody(final URI uri) {
+  public HttpGetWithBody(final HttpRequestBase http) {
     super();
-    setURI(uri);
-  }
-
-  /**
-   * @throws IllegalArgumentException if the uri is invalid.
-   */
-  public HttpGetWithBody(final String uri) {
-    super();
-    setURI(URI.create(uri));
+    setURI(http.getURI());
+    this.setHeaders(http.getAllHeaders());
   }
 
   @Override
