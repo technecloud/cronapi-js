0d5eac988a751659308d06d87949efc7ab914eeb
Augusto Luiz Santos Queiroz <augustolsq@gmail.com>
2020-11-19 18:17:34 -0300

    feature/CRONAPP-2810: Cobertura de teste unitário nas funções createChart, createDataset

---
+++ test/src/test/cronapi/chart/chart.spec.ts
@@ -0,0 +1,14 @@
+describe('Test suit for category chart from Cronapi.js', function() {
+  const ch = require('chai');
+  ch.should();
+
+  let {window} = require('../../../../../cronapi');
+  const cronapi = window["cronapi"];
+
+  it('createChart', function() {
+  });
+
+  it('createDataset', function() {
+  });
+
+});
