06c969c9575fe3c9a6c718583d9eb7b0a8d8a751
Augusto Luiz Santos Queiroz <augustolsq@gmail.com>
2020-11-23 12:39:26 -0300

    feature/CRONAPP-2830: Cobertura de teste unitário na função getParentNode feature/CRONAPP-2830: Cobertura de teste unitário na função setElementContent feature/CRONAPP-2830: Cobertura de teste unitário na função getElementContent feature/CRONAPP-2830: Cobertura de teste unitário na função removeElement feature/CRONAPP-2830: Cobertura de teste unitário na função getElementName

--- test/src/test/cronapi/xml/xml.spec.ts
+++ test/src/test/cronapi/xml/xml.spec.ts
@@ -53,18 +53,31 @@ describe('Test suit for category xml from Cronapi.js', function() {
   });
 
   it('getParentNode', function() {
+    let value = cronapi.xml.getParentNode($.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'));
+    (value === undefined || value === null || value === '').should.equal(true);
   });
 
   it('setElementContent', function() {
+    let value = cronapi.xml.setElementContent($.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'), $.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'));
+    (value === undefined || value === null || value === '').should.equal(true);
   });
 
   it('getElementContent', function() {
+    let value = cronapi.xml.getElementContent($.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'));
+    (value === undefined || value === null || value === '').should.equal(true);
   });
 
   it('removeElement', function() {
+    let value = cronapi.xml.removeElement($.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'), true);
+    (value === undefined || value === null || value === '').should.equal(true);
+
+    value = cronapi.xml.removeElement($.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'), false);
+    (value === undefined || value === null || value === '').should.equal(true);
   });
 
   it('getElementName', function() {
+    let value = cronapi.xml.getElementName($.parseXML('<?xml version="1.0" encoding="UTF-8"?><root></root>'));
+    (value === undefined || value === null || value === '').should.equal(true);
   });
 
   it('renameElement', function() {
