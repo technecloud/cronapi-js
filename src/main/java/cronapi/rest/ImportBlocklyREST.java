636523853a438be1455808a1cd144d5329fb8eba
Michel Moreira <michel@michelmoreira.eti.br>
2020-12-07 11:36:56 -0300

    CRONAPP-2608 Gerar jar de um sistema criado com o Cronapp e rodar (#184)
    * CRONAPP-2675  Gerar jar de um sistema criado com o Cronapp e rodar (#158)
    
    * CRONAPP-2675  Gerar jar de um sistema criado com o Cronapp e rodar (#161)
    
    * CRONAPP-2675  Gerar jar de um sistema criado com o Cronapp e rodar (#165)
    
    Problema
    
    Fora do jar não listava locales
    Não estava listando as cronapis dos projetos
    Solução
    
    Relativizar o caminho dos locales antes de processar
    Inverter a condição de validar o caminho da cronapi
    
    * CRONAPP-2608 Gerar jar de um sistema criado com o Cronapp e rodar

--- src/main/java/cronapi/rest/ImportBlocklyREST.java
+++ src/main/java/cronapi/rest/ImportBlocklyREST.java
@@ -1,110 +1,182 @@
 package cronapi.rest;
 
+import com.google.gson.JsonObject;
 import cronapi.AppConfig;
-import java.io.File;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import cronapi.util.Operations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.JsonObject;
-import cronapi.util.Operations;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(value = "/js/blockly.js")
 public class ImportBlocklyREST {
 
+  private static final List<String> API_PATHS = Arrays.asList("cronapi-js", "cronapp-framework-js", "cronapp-framework-mobile-js");
   private static List<String> imports;
-  private static boolean isDebug = Operations.IS_DEBUG;
-  private final static List<String> API_PATHS = new ArrayList<String>(){{add("cronapi-js");add("cronapp-framework-js");add("cronapp-framework-mobile-js");}};
-  private static List<String> localesKeys = new ArrayList<String>();
-  private static JsonObject localesRef  = new JsonObject();
+  private static volatile boolean shouldInitializeImports = true;
+  private static List<String> localesKeys = new ArrayList<>();
+  private static JsonObject localesRef = new JsonObject();
 
-  private void fill(String base, File folder, List<String> imports) {
-    for(File file : folder.listFiles(  (d,s) ->{
-       boolean valid = true;
-        for(String api : API_PATHS){
-            if(d.getName().contains(api)){
-                valid = false;
-                break;
-            }
-        }
-      return valid;
-    } )) {
-      if(file.isDirectory()) {
-        fill(base, file, imports);
-      }
-      else {
-        if(file.getName().endsWith(".blockly.js")) {
-          String js = file.getAbsolutePath().replace(base, "");
-          js = js.replace("\\", "/");
-          if(js.startsWith("/")) {
-            js = js.substring(1);
-          }
-          imports.add(js + "?" + file.lastModified());
-        }
-      }
+  private final Logger logger;
+
+  public ImportBlocklyREST(Optional<Logger> logger) {
+    this.logger = logger.orElse(Logger.getLogger(this.getClass().getName()));
+  }
+
+  private static boolean isValidApiPath(Path path) {
+    String pathString = path.toString();
+    return API_PATHS.stream().noneMatch(pathString::contains);
+  }
+
+  private static boolean isValidLocalePath(String path) {
+    return path.startsWith("locale") && path.endsWith(".json");
+  }
+
+  private static void fillLanguages(Path folder) throws IOException {
+    localesKeys = new ArrayList<>();
+    localesRef = new JsonObject();
+    Path i18nPath = folder.resolve("i18n");
+
+    if (!Files.exists(i18nPath)) {
+      return;
+    }
+
+    try (Stream<Path> stream = Files.walk(i18nPath)) {
+      stream
+          .map(path -> path.getFileName().toString())
+          .filter(ImportBlocklyREST::isValidLocalePath)
+          .forEach(ImportBlocklyREST::addLocale);
     }
   }
 
-  @RequestMapping(method = RequestMethod.GET)
-  public void listBlockly(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  private static void addLocale(String path) {
+    String localeName = path.substring(7, path.length() - 5);
+    fillLanguageSet(localeName);
+  }
+
+  private static void fillLanguageSet(String localeName) {
+    if (!localesKeys.contains(localeName)) {
+      localesKeys.add(localeName);
+    }
+    localesRef.addProperty(localeName.substring(0, 2) + "*", localeName);
+    if (localesRef.get("*") == null) {
+      localesRef.addProperty("*", localeName);
+    }
+    if (localeName.equals("pt_br")) {
+      localesRef.addProperty("*", localeName);
+    }
+  }
+
+  private void fill(Path base, List<String> imports) throws IOException {
+    try (Stream<Path> stream = Files.walk(base)) {
+      stream
+          .filter(ImportBlocklyREST::isValidApiPath)
+          .forEach(candidate -> addImport(base, candidate, imports));
+    }
+  }
+
+  private void addImport(Path base, Path path, List<String> imports) {
+    String hash = "";
+
+    try {
+      BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
+
+      if (attributes.isDirectory()) {
+        return;
+      }
+
+      FileTime lastModifiedTime = attributes.lastModifiedTime();
+      if (lastModifiedTime != null) {
+        hash = "?" + attributes.lastModifiedTime().toMillis();
+      }
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Error getting path attributes", e);
+    }
+
+
+    String pathString = base.relativize(path).toString();
+
+    if (pathString.endsWith(".blockly.js")) {
+      String js = pathString.replace("\\", "/");
+      if (js.startsWith("/")) {
+        js = js.substring(1);
+      }
+
+      imports.add(js + hash);
+    }
+  }
+
+  @GetMapping()
+  public void listBlockly(HttpServletRequest request, HttpServletResponse response) throws IOException, URISyntaxException {
     response.setContentType("application/javascript");
     PrintWriter out = response.getWriter();
-    if(imports == null) {
-      synchronized(ImportBlocklyREST.class) {
-        if(imports == null) {
+    if (shouldInitializeImports) {
+      synchronized (ImportBlocklyREST.class) {
+        if (shouldInitializeImports) {
           List<String> fillImports = new ArrayList<>();
-          File folder = new File(request.getServletContext().getRealPath("/"));
-          fill(request.getServletContext().getRealPath("/"), folder, fillImports);
-          if(!isDebug) {
-            imports = fillImports;
+          FileSystem fileSystem = null;
+
+          Path folderPath;
+          ServletContext servletContext = request.getServletContext();
+          String folderRealPath = servletContext.getRealPath("/");
+
+          if (folderRealPath == null) {
+            URI folderUri = request.getServletContext().getResource("/").toURI();
+
+            Map<String, String
+                > env = new HashMap<>();
+            env.put("create", "true");
+
+            fileSystem = FileSystems.newFileSystem(folderUri, env);
+            folderPath = Paths.get(folderUri);
+          } else {
+            folderPath = Paths.get(folderRealPath);
           }
-          else {
-            fillLanguages(folder);
-            write(out, fillImports);
+
+          try {
+            fill(folderPath, fillImports);
+            if (!Operations.IS_DEBUG) {
+              imports = fillImports;
+              shouldInitializeImports = false;
+            } else {
+              fillLanguages(folderPath);
+              write(out, fillImports);
+            }
+          } finally {
+            if (fileSystem != null) {
+              fileSystem.close();
+            }
           }
         }
       }
     }
 
-    if(imports != null) {
+    if (imports != null) {
       write(out, imports);
     }
   }
 
-  private void fillLanguages(File folder){
-    localesKeys = new ArrayList<String>();
-    localesRef = new JsonObject();
-
-    File folderI18n = new File(folder, "i18n");
-
-    if (folderI18n.exists()) {
-      for(File filee : folderI18n.listFiles(  (d,s) ->{
-        boolean valid = false;
-        if(s.startsWith("locale") && s.endsWith(".json")){
-          valid = true;
-          return valid;
-        }
-        return valid;
-      } )) {
-        String localeName = filee.getName().substring(7,  filee.getName().length() - 5);
-        fillLanguageSet(localeName);
-      }
-    }
-  }
-
   private void write(PrintWriter out, List<String> imports) {
-    String localesKeysString = arrayToString(localesKeys)  + ";";
-    String localesRefString = localesRef.toString()  + ";";
-    out.println("window.fixedTimeZone = "+ AppConfig.fixedTimeZone() +";");
-    out.println("window.timeZone = '"+ AppConfig.timeZone() +"';");
-    out.println("window.timeZoneOffset = "+ AppConfig.timeZoneOffset() +";");
+    String localesKeysString = arrayToString(localesKeys) + ";";
+    String localesRefString = localesRef.toString() + ";";
+    out.println("window.fixedTimeZone = " + AppConfig.fixedTimeZone() + ";");
+    out.println("window.timeZone = '" + AppConfig.timeZone() + "';");
+    out.println("window.timeZoneOffset = " + AppConfig.timeZoneOffset() + ";");
     out.println("window.blockly = window.blockly || {};");
     out.println("window.blockly.js = window.blockly.js || {};");
     out.println("window.blockly.js.blockly = window.blockly.js.blockly || {};");
@@ -112,32 +184,19 @@ public class ImportBlocklyREST {
     out.println("window.translations.localesKeys = " + localesKeysString);
     out.println("window.translations.localesRef =  " + localesRefString);
 
-    for(String js : imports) {
+    for (String js : imports) {
       out.println("document.write(\"<script src='" + js + "'></script>\")");
     }
   }
 
-  private void fillLanguageSet(String localeName){
-    if(localesKeys.indexOf(localeName) == -1){
-      localesKeys.add(localeName);
-    }
-    localesRef.addProperty(localeName.substring(0,2) + "*", localeName);
-    if(localesRef.get("*") == null){
-      localesRef.addProperty("*", localeName);
-    }
-    if(localeName.equals("pt_br")){
-      localesRef.addProperty("*", localeName);
-    }
-  }
-
-  private String arrayToString(List<String> stringList){
+  private String arrayToString(List<String> stringList) {
     StringBuilder b = new StringBuilder();
     b.append("[");
-    for(String key : stringList){
-      if(stringList.indexOf(key) != 0) {
+    for (String key : stringList) {
+      if (stringList.indexOf(key) != 0) {
         b.append(",");
       }
-      b.append("'" + key + "'");
+      b.append("'").append(key).append("'");
     }
     b.append("]");
     return b.toString();
