d5a86a21f67c018dff6cdbee0efc1da9e20fec51
lincolnminto-cronapp <36731613+lincolnminto-cronapp@users.noreply.github.com>
2021-04-08 20:03:33 -0300

    CRONAPP-3609 - Login SSO (OAuth2) implementação em projetos PWA (#174)
    Solução
    Caso a aplicação é web ou mobile na web (PWA) utilizar opção de abrir em uma nova janela onde no framework mobile se espera o message postado pelo framework-java

--- cronapi.js
+++ cronapi.js
@@ -4893,10 +4893,8 @@ if (!window.fixedTimeZone) {
 
         ref.addEventListener('loadstart', handlerChange);
 
-      } else if(window.ionic && window.hostApp) {
-        window.location = this.cronapi.internal.getAddressWithHostApp("login");
       } else {
-        window.location.assign("login");
+        window.ssoWindow = window.open(this.cronapi.internal.getAddressWithHostApp("login"));
       }
   };
 
