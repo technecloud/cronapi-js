3952f83be294e9e0623bd66d4ace53c1c87dc440
Ronélio Oliveira <ronelio.oliveira@gmail.com>
2021-04-19 13:14:46 -0300

    Feature/CRONAPP-3573 - Uso de QRCode com PWA
    https://cronapp.atlassian.net/browse/CRONAPP-3573
    
    ## Problema:
    Rotinas de QRCode e código de barras não funcionam em PWA
    ## Melhoria:
    
    ## Solução:
    Rotinas implementadas para PWA utilizando a biblioteca https://zxing-js.github.io/library/

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
-    minify = require("gulp-babel-minify");
+    minify = require("gulp-babel-minify"),
+    concat = require('gulp-concat');
 
 gulp.task('minify', function () {
     return gulp.src('cronapi.js')	
@@ -15,10 +16,16 @@ gulp.task('minify', function () {
         .pipe(gulp.dest('dist/'));
 });
 
+gulp.task('joinfiles', ['minify'],  function() {
+  return gulp.src(['dist/cronapi.min.js', 'node_modules/@zxing/library/umd/index.min.js'])
+    .pipe(concat('cronapi.min.js'))
+    .pipe(gulp.dest('dist/'));
+});
+
 gulp.task('i18n', function () {
     return gulp.src('i18n/').pipe(gulp.dest('i18n/'));
 });
 
-gulp.task('build', ['minify', 'i18n']);
+gulp.task('build', ['minify', 'i18n', 'joinfiles']);
 
 gulp.task('default', ['build']);
